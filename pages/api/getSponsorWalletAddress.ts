861fc8943633bd9af3ff716d15c474980543a8b7
nana <ajibaa@gmail.com>
2024-02-04 20:55:46 +0530

    WIP, build fails

---
+++ pages/api/getSponsorWalletAddress.ts
@@ -0,0 +1,31 @@
+import type { NextRequest } from "next/server";
+import { deriveSponsorWalletAddress } from "@api3/airnode-admin";
+
+// ------------------
+// Using Crypto with Edge Middleware and Edge Functions
+// ------------------
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req: NextRequest) {
+  const body = await req.json();
+
+  const { airnodeAddress, airnodeXpub, contractAddress } = JSON.parse(
+    JSON.stringify(body),
+  );
+
+  const sponsorWalletAddress = deriveSponsorWalletAddress(
+    airnodeXpub,
+    airnodeAddress,
+    contractAddress,
+  );
+
+  return new Response(
+    JSON.stringify({
+      sponsorWalletAddress,
+    }),
+    { headers: { "Content-Type": "application/json" } },
+  );
+}
